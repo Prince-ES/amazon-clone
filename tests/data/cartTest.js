cf79c16c17433645e2d8dc0416e4121f2e9561ec
Prince <mauryaramlaxman42@gmail.com>
2024-07-06 11:22:18 +0530

    Rename tests-jasmine folder to tests

--- tests/data/cartTest.js
+++ tests/data/cartTest.js
@@ -12,7 +12,6 @@ describe('test suite: addToCart',()=>{
     });
       loadFromStorage();//this will give the cart value from above spyOn localStorage getItem;
       addToCart('15b6fc6f-327a-4ec4-896f-486349e85a3d');
-      console.log(cart[0]);
       expect(cart.length).toEqual(1);
       expect(localStorage.setItem).toHaveBeenCalledTimes(1);
       expect(cart[0].productId).toEqual('15b6fc6f-327a-4ec4-896f-486349e85a3d');
